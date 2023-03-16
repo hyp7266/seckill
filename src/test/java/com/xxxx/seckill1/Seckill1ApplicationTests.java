4f086beb39c99a137380d066b006f503d0385351
hyp7266 <95162340+hyp7266@users.noreply.github.com>
2023-03-16 14:04:21 +0800

    Add files via upload

---
+++ src/test/java/com/xxxx/seckill1/Seckill1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxxx.seckill1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seckill1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
